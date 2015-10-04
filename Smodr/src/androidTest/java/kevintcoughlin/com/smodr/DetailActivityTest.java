2d38e47f0a6afb416e26ea529403b9de256d7d3f
Kevin Coughlin <kevintcoughlin@gmail.com>
2015-10-03 20:59:49 -0400

    Add DetailActivity test with view checks

--- Smodr/src/androidTest/java/kevintcoughlin/com/smodr/DetailActivityTest.java
+++ Smodr/src/androidTest/java/kevintcoughlin/com/smodr/DetailActivityTest.java
@@ -4,7 +4,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 import com.kevintcoughlin.smodr.R;
-import com.kevintcoughlin.smodr.views.activities.MainActivity;
+import com.kevintcoughlin.smodr.views.activities.DetailActivity;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,13 +16,18 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class MainActivityTest {
+public class DetailActivityTest {
 	@Rule
-	public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
-			MainActivity.class);
+	public ActivityTestRule<DetailActivity> mActivityRule = new ActivityTestRule<>(
+			DetailActivity.class);
 
 	@Test
 	public void layout_viewsExist() {
+		onView(withId(R.id.main_content)).check(matches(isDisplayed()));
+		onView(withId(R.id.appbar)).check(matches(isDisplayed()));
+		onView(withId(R.id.collapsing_toolbar)).check(matches(isDisplayed()));
+		onView(withId(R.id.backdrop)).check(matches(isDisplayed()));
+		onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
 		onView(withId(R.id.list)).check(matches(isDisplayed()));
 	}
 }
