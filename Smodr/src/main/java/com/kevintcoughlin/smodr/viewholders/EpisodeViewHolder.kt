55f371de48d7ce74f0fdad76dd7fd759d2e52730
Kevin T. Coughlin <706967+KevinTCoughlin@users.noreply.github.com>
2024-11-24 14:56:44 -0800

    Refactor

--- Smodr/src/main/java/com/kevintcoughlin/smodr/viewholders/EpisodeViewHolder.kt
+++ Smodr/src/main/java/com/kevintcoughlin/smodr/viewholders/EpisodeViewHolder.kt
@@ -1,15 +1,10 @@
 package com.kevintcoughlin.smodr.viewholders
 
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.kevintcoughlin.smodr.databinding.ItemListEpisodeLayoutBinding
 
-class EpisodeViewHolder internal constructor(binding: ItemListEpisodeLayoutBinding) :
-    RecyclerView.ViewHolder(binding.root) {
-    @JvmField
-    var mTitle: TextView = binding.title
-    @JvmField
-    var mMetadata: TextView = binding.metadata
-    @JvmField
-    var mDescription: TextView = binding.description
+class EpisodeViewHolder(binding: ItemListEpisodeLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    val mTitle = binding.title
+    val mMetadata = binding.metadata
+    val mDescription = binding.description
 }
