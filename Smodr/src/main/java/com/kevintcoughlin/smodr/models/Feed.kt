d0362e6d18cec801b0aa91be340467532c33fc70
Kevin T. Coughlin <706967+KevinTCoughlin@users.noreply.github.com>
2024-11-24 21:06:56 -0800

    Refactor

--- Smodr/src/main/java/com/kevintcoughlin/smodr/models/Feed.kt
+++ Smodr/src/main/java/com/kevintcoughlin/smodr/models/Feed.kt
@@ -4,7 +4,6 @@ import com.tickaroo.tikxml.annotation.Element
 import com.tickaroo.tikxml.annotation.Xml
 
 @Xml(name = "rss")
-class Feed {
-    @field:Element
-    var channel: Channel? = null
-}
+data class Feed(
+    @field:Element var channel: Channel = Channel()
+)
